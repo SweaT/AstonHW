536b7607b65a95b2537ff6153eb73a1e2bc6c150
Zelepukin Anton <flifezel@yandex.ru>
2024-08-18 20:02:11 +0300

    Merged two branches into master

--- src/Main.java
+++ src/Main.java
@@ -2,24 +2,6 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        // затестить isEmpty() and Clear() and GetIndex() remove(T obj)
-
-        MyArrayList<String> myAL = new MyArrayList<>(List.of("asd", "123", "Cool Beavers", "Norman", "Gerbert"));
-
-        System.out.println(myAL);
-        System.out.println(myAL.getSize());
-
-        myAL.remove("123");
-        System.out.println(myAL);
-        System.out.println(myAL.getSize());
-        myAL.remove("gerbert");
-
-        System.out.println(myAL);
-        System.out.println(myAL.getSize());
-
-
-
-
         MyArrayList<Integer> ints = new MyArrayList<>(List.of(8,12,-5,1004,22,-1000, 1,0));
 
         System.out.println(ints);
@@ -27,6 +9,5 @@ public class Main {
         ints = MyQuickSort.sort(ints);
 
         System.out.println(ints);
-
     }
 }
